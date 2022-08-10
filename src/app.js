15b25a9cb1a817bd938c88ac90f632f169b27284
Liana <l.yukhymchak@gmail.com>
2022-08-11 00:56:55 +0300

    Updated current day and time display

--- src/app.js
+++ src/app.js
@@ -10,8 +10,37 @@ function displayTemperature(response) {
   descriptionElement.innerHTML = response.data.weather[0].description;
   let currentCity = document.querySelector("#current-city");
   currentCity.innerHTML = response.data.name;
+
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatTime(response.data.dt * 1000);
 }
 
+function getCurrentDay(timeStamp) {
+  let date = new Date(timeStamp);
+  let day = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  return days[day];
+}
+function getCurrentTime(timeStamp) {
+  let date = new Date(timeStamp);
+  let hours = date.getHours();
+  hours = hours < 10 ? "0" + hours : hours;
+  let minutes = date.getMinutes();
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  return `${hours}:${minutes}`;
+}
+
+function formatTime(timeStamp) {
+  return `${getCurrentDay(timeStamp)} ${getCurrentTime(timeStamp)}`;
+}
 let apiKey = "0bf9a64f249d8b9bdf366b82bcb3cbf3";
 let unit = "metric";
 let city = "Tallinn";
